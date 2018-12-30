2698ba1c06899e9c772106080612ecaafb06f4ca
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-12-31 01:54:42 +0300

    Добавили предложение на публикацию фото на стене сообщества

--- vkcloud.py
+++ vkcloud.py
@@ -3,8 +3,10 @@ import argparse, random, io, re, os, time, requests
 from PIL import Image, ImageDraw, ImageFont
 from google.cloud import vision
 
-VK_TOKEN = "VK_TOKEN"
+USER_TOKEN = "USER_TOKEN"
+CLUB_TOKEN = "CLUB_TOKEN"
 TTF_DIR = "~/Library/Fonts/MuseoSansCyrl-300.ttf"
+CLUB_ID = "-175867271"
 
 def getfile(url):
 	thisfilename = str(uuid.uuid4())+".jpg"
@@ -43,14 +45,42 @@ def localize_objects(path):
     return obj_of_objects
 
 def objects_formater(obj):
-	out_str = "Ваши результаты:\n" 
+	out_str = "" 
 	for key in obj:
 		out_str += key+" "+str(int(round(obj[key],2)*100))+"%\n"
 	return out_str
 
 def main():
+
+	main_obj = {}
 	#Основная конфигурация
-	VKSession = vk.Session(access_token=VK_TOKEN)
+	VKSession = vk.Session(access_token=CLUB_TOKEN)
+
+	request_keyboard = {
+	"one_time":True,
+	"buttons":[
+		[
+			{
+			"action":{
+				"type":"text",
+				"payload":"{\"button\": \"1\"}",
+				"label":"Да"
+			},
+			"color":"primary"
+			},
+
+			{
+			"action":{
+				"type":"text",
+				"payload":"{\"button\": \"2\"}",
+				"label":"Нет"
+			},
+			"color":"negative"
+			},
+
+		]
+	]
+	}
 
 	api = vk.API(VKSession)
 	APIVersion = 5.73
@@ -107,7 +137,28 @@ def main():
 
 			#Чекаем входящие сообщения
 			if message_longpoll != [0]:
-				if attaches["attach1_type"] == "photo":
+
+				if message_longpoll == "Да":
+					api.messages.send(user_id=chat_longpoll,message="Готово ✨",v=APIVersion)
+
+					UserVKSession = vk.Session(access_token=USER_TOKEN)
+					UserAPI = vk.API(UserVKSession)
+					user_server_url = UserAPI.photos.getWallUploadServer(group_id=CLUB_ID[1:],v=APIVersion)["upload_url"]
+
+					user_photo_response = requests.post(user_server_url,files={'photo': open(main_obj[chat_longpoll][0], 'rb')}).json()
+					user_photo_final = UserAPI.photos.saveWallPhoto(group_id=CLUB_ID[1:],photo=user_photo_response["photo"],server=user_photo_response["server"],hash=user_photo_response["hash"],caption=main_obj[chat_longpoll][1],v=APIVersion)[0]
+					user_photo_str = "photo"+str(user_photo_final["owner_id"])+"_"+str(user_photo_final["id"])
+					
+					UserAPI.wall.post(owner_id=CLUB_ID,from_group=1,message=main_obj[chat_longpoll][1],attachments=user_photo_str,v=APIVersion)
+					os.remove(main_obj[chat_longpoll][0])
+					main_obj.pop(chat_longpoll, None)
+
+				elif message_longpoll == "Нет":
+					api.messages.send(user_id=chat_longpoll,message="Хорошо 😌",v=APIVersion)
+					os.remove(main_obj[chat_longpoll][0])
+					main_obj.pop(chat_longpoll, None)
+
+				elif attaches["attach1_type"] == "photo":
 					photo_json = api.messages.getById(message_ids=response['updates'][0][1],v=APIVersion)["items"][0]["attachments"][0]["photo"]
 							
 					#Простите
@@ -115,13 +166,15 @@ def main():
 					for key in photo_json:
 						if key[:5] == "photo":
 							keyname = key
+
 					server_url = api.photos.getMessagesUploadServer(peer_id=chat_longpoll,v=APIVersion)["upload_url"]
 					thisfilename = getfile(photo_json[keyname])
 					message_final = objects_formater(localize_objects(thisfilename))
 					photo_response = requests.post(server_url,files={'photo': open(thisfilename, 'rb')}).json()
 					photo_final = api.photos.saveMessagesPhoto(photo=photo_response["photo"],server=photo_response["server"],hash=photo_response["hash"],v=APIVersion)[0]
-					api.messages.send(user_id=chat_longpoll,message=message_final,attachment="photo"+str(photo_final["owner_id"])+"_"+str(photo_final["id"]),v=APIVersion)
-					os.remove(thisfilename)
+					photo_str = "photo"+str(photo_final["owner_id"])+"_"+str(photo_final["id"])
+					api.messages.send(user_id=chat_longpoll,message="Ваши результаты:\n"+message_final+"\nХотите поделиться этим фото в сообществе?",attachment=photo_str,keyboard=json.dumps(request_keyboard,ensure_ascii=False),v=APIVersion)
+					main_obj[chat_longpoll]=[thisfilename, message_final]
 
 if __name__ == '__main__':
 	main()
