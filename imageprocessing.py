b41b35dd432652499aa6b86a6260dc8bade5c57e
Georgiy Demo <demenchuk.george@protonmail.com>
2019-12-24 01:27:10 +0300

    Оптимайз алгоритма отдельно в модуле

--- imageprocessing.py
+++ imageprocessing.py
@@ -1,8 +1,10 @@
-import time, json, uuid, random, io, re, os, time, requests
+import random
+
 from PIL import Image, ImageDraw, ImageFont
 from google.cloud import vision
 
 TTF_DIR = "./MuseoSansCyrl-300.ttf"
+
 
 class PhotoProcessing():
 
@@ -14,9 +16,9 @@ class PhotoProcessing():
 
     def string_formater(self):
         all_objects = self.results
-        out_str = "" 
+        out_str = ""
         for key in all_objects:
-            out_str += key+" "+str(int(round(all_objects[key],2)*100))+"%\n"
+            out_str += key + " " + str(int(round(all_objects[key], 2) * 100)) + "%\n"
         self.results = out_str
 
     def localize_objects(self):
@@ -26,36 +28,36 @@ class PhotoProcessing():
         obj_of_objects = {}
         allcoords = []
 
-        #Открытие изображения    
+        # Открытие изображения
         im = Image.open(path)
         draw = ImageDraw.Draw(im)
-        
+
         client = vision.ImageAnnotatorClient()
-        
+
         with open(path, 'rb') as image_file:
             content = image_file.read()
             image = vision.types.Image(content=content)
 
-        #Все объекты
+        # Все объекты
         objects = client.object_localization(image=image).localized_object_annotations
 
-        #Цикл по каждому объекту
+        # Цикл по каждому объекту
         for obj in objects:
 
-            obj_of_objects[obj.name]=obj.score
-            box = [(vertex.x*im.width, vertex.y*im.height) for vertex in obj.bounding_poly.normalized_vertices]
-                    
+            obj_of_objects[obj.name] = obj.score
+            box = [(vertex.x * im.width, vertex.y * im.height) for vertex in obj.bounding_poly.normalized_vertices]
+
             if box not in allcoords:
                 allcoords.append(box)
-                r = lambda: random.randint(0,255)
-                draw.line(box + [box[0]], width=5, fill='#%02X%02X%02X' % (r(),r(),r()))
-                draw.text(box[0], obj.name+" "+str(obj.score), font=ImageFont.truetype(TTF_DIR, 30))
-        
+                r = lambda: random.randint(0, 255)
+                draw.line(box + [box[0]], width=5, fill='#%02X%02X%02X' % (r(), r(), r()))
+                draw.text(box[0], obj.name + " " + str(obj.score), font=ImageFont.truetype(TTF_DIR, 30))
+
         self.image = im
         self.results = obj_of_objects
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     thisfilename = "15763474458860.png"
     detector = PhotoProcessing(thisfilename)
     detector.image.save(thisfilename)
